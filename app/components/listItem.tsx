d572a8e2a44a6ba4e3d6efdf73cfc02d1952f543
hnggngn <rozephyrus@gmail.com>
2022-01-19 22:56:51 +0700

    fix text not show properly on mobile

--- app/components/listItem.tsx
+++ app/components/listItem.tsx
@@ -12,13 +12,15 @@ const ListItemComponent = ({ todos }: IListItemProps) => {
     return (
         <HStack w="100%">
             <Stack
+                w="100%"
                 p={3}
                 rounded={10}
                 as="button"
                 onClick={() => isCompleted(todos)}
-                w="100%"
                 color={todos.complete ? 'gray.400' : 'orange.500'}
-                bg={todos.complete ? 'gray.100' : 'orange.50'}>
+                bg={todos.complete ? 'gray.100' : 'orange.50'}
+                wordBreak="break-word"
+                textAlign="start">
                 <Text fontWeight={500} fontFamily="Inter">
                     Created on: {todos.createdOn}
                 </Text>
@@ -26,17 +28,12 @@ const ListItemComponent = ({ todos }: IListItemProps) => {
                     textDecoration={todos.complete ? 'line-through' : 'none'}
                     fontSize="lg"
                     fontFamily="Inter"
-                    fontWeight={600}
-                    overflow="hidden"
-                    textOverflow="ellipsis"
-                    whiteSpace="nowrap">
+                    fontWeight={600}>
                     {todos.title}
                 </Text>
-                {todos.complete && (
-                    <Text fontWeight={500} fontFamily="Inter">
-                        Completed on: {todos.completedOn}
-                    </Text>
-                )}
+                <Text fontWeight={500} fontFamily="Inter">
+                    {todos.complete && `Completed on: ${todos.completedOn}`}
+                </Text>
             </Stack>
             <IconButton
                 onClick={() => removeTodo(todos)}
